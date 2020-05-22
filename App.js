ecc54476502053e2e973c2a449851688337f7fb5
viguime <jvictormguimaraes@gmail.com>
2020-05-22 03:49:20 -0300

    Distance on the screen

--- App.js
+++ App.js
@@ -1,34 +1,82 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet,Button, Text, View, DrawerLayoutAndroidBase } from 'react-native';
 import { DeviceEventEmitter } from 'react-native'
 import Beacons from 'react-native-beacons-manager'
 
+//Beacons.requestAlwaysAuthorization
 // Tells the library to detect iBeacons
 Beacons.detectIBeacons()
 
-// Start detecting all iBeacons in the nearby
+  export default function App() {
+
+  const [dados, setDados] = useState([]);
+  const [dist, setDist] = useState(0);
+  const [deltadist,setDeltatdist] = useState(0);
+
+
+ 
 async function rebea(){
-try {
-  await Beacons.startRangingBeaconsInRegion('REGION1')
-  console.log(`Beacons ranging started succesfully!`)
-} catch (err) {
-  console.log(`Beacons ranging not started, error: ${error}`)
+  try {
+    await Beacons.startRangingBeaconsInRegion('REGION1')
+    console.log(`Beacons ranging started succesfully!`)
+  }catch (err) {
+    console.log(`Beacons ranging not started, error: ${error}`)
+  }
 }
-}
-rebea()
 
-// Print a log of the detected iBeacons (1 per second)
-DeviceEventEmitter.addListener('beaconsDidRange', (data) => {
-  console.log('Found beacons!', data.beacons)
+
+function zera(){
+  setDist(0)
+  setDeltatdist(0)
+}
+
+useEffect(() => {
+
+  DeviceEventEmitter.addListener('beaconsDidRange', (data) => {
+ // console.log('Found beacons!', data.beacons)
+  setDados(data.beacons)
 })
+},[]);
 
-export default function App() {
+dados.map(Element=>
+  {console.log('sinal',Element.distance)
+
+  if (((Element.distance-dist)>0.7)||((dist-Element.distance)>0.7)){
+    setDist(Element.distance)
+  }
+  
+});
+
+
+
+console.log('distancia',dist)
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style = {styles.container}>
+        <Button
+          title = "Connect"
+          onPress = {rebea}
+        />
+      </View>
+
+      <View style = {styles.container}>      
+        <Button
+          title = "Zerar"
+          onPress = {zera}
+        />
+      </View>
+      
+      <View style = {styles.container}>
+          <Text>
+          Distância: {dist.toFixed(2)}m
+          </Text>
+      </View>
     </View>
   );
 }
+  
 
 const styles = StyleSheet.create({
   container: {
@@ -36,5 +84,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 30,
   },
+ 
 });
